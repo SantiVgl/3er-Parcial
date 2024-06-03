dfd8168878394fd4bcc65852439f5a919c1e546e
Santi Varela Guzmán <168608359+SantiVgl@users.noreply.github.com>
2024-06-03 15:07:28 -0600

    Hice las correcciones

--- PsC++1.cpp
+++ PsC++1.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-
+using namespace std;
 int main() {
     int num[5];
 
-    for (int i = 0; i < 5; i++) {
-        std::cout << "Digite un numero para la posicion " << i << ": ";
-        std::cin >> num[i];
+    for (int i=0;i<5;i++) {
+        cout<<"Digite un numero para la posicion " << i << ": ";
+        cin>>num[i];
     }
 
-    for (int i = 0; i < 5; i++) {
-        std::cout << "El dato en la posicion " <<i<< " exportes: " << num[i] << std::endl;
+    for (int i =0;i<5;i++) {
+        cout<<"El dato en la posicion " <<i<< " es: " << num[i] <<endl;
     }
 
     return 0;
