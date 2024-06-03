aa50bd354a086119fd2b0287b42937865bfbfd13
Santi Varela Guzmán <168608359+SantiVgl@users.noreply.github.com>
2024-06-03 15:01:54 -0600

    Hice las correcciones

--- For1C++.cpp
+++ For1C++.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
+using namespace std;
 
 int main() 
 {
     for (int i = 1; i <= 100; ++i)
     {
-        std::cout << i << std::endl;
+        cout<<i<<endl;
     }
 
     return 0;
